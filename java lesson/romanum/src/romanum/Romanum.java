0ab909a6d5f947fdb29a73fbde4c876e4957dfa6
decemberC <wanghoc71@gmail.com>
2018-12-23 21:00:28 +0800

    add more on romanum(still has bug)

--- java lesson/romanum/src/romanum/Romanum.java
+++ java lesson/romanum/src/romanum/Romanum.java
@@ -5,10 +5,20 @@ public class Romanum {
   
  }
  public static void main(String[] args) {
-  Scanner iner = new Scanner(System.in);
-  try{
-	  
-  }catch(Exception wrongin){
- }
+	 boolean yes = false;
+	 Scanner iner = new Scanner(System.in);
+	 while (!yes) {
+	 Integer needchange = null;
+	 try {
+	  System.out.print("please enter a number between 0 to 1001: ");
+	  needchange = iner.nextInt();
+      if(needchange >0 && needchange<1000) {
+		yes =true; 
+	 }
+	}catch(Exception wrongin){
+		 System.out.println("Error, input should between 0 to 1001");
+		 break;
+	}
+  }
 }
 }
