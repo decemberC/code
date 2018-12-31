5093c6af938bb0ca2a15f2d2c7190cfed4cf0379
decemberC <wamghoc71@gmail.com>
2018-12-31 18:08:24 +0800

    Beta 0.9

--- java lesson/romanum/src/romanum/Romanum.java
+++ java lesson/romanum/src/romanum/Romanum.java
@@ -14,14 +14,97 @@ public class Romanum {
 	        return output;	 
 	}
 	if (starget<5){
-		if
+		if(starget>3){
+			output = one+five;
+			return output;
+		}else{
+			for (int i = 1,i<=starget,i++){
+				output = output + one;
+			}
+			return output;
+		}
+	}else{
+		if (starget>8){
+			output = one +ten;
+			return output;
+		}else{
+			output=five;
+			for (int i = 1,i<=starget%5,i++){
+				output=output+one;
+			}
+			return output;
+		}
 	}
  }
  private String exchange(int starget,int dtarget){
-	 
+		char one,five,ten;
+	String output = new String();
+	one = 'X';
+	five = 'L';
+    ten = 'C';
+	switch (dtarget){
+	 case 1: output=one;
+	         return output+exchange(starget);
+     case 5:output=five;
+	        return output+exchange(starget);	 
+	}
+	if (dtarget<5){
+		if(dtarget>3){
+			output = one+five;
+			return output+exchange(starget);
+		}else{
+			for (int i = 1,i<=dtarget,i++){
+				output = output + one;
+			}
+			return output+exchange(starget);
+		}
+	}else{
+		if (dtarget>8){
+			output = one +ten;
+			return output+exchange(starget);
+		}else{
+			output=five;
+			for (int i = 1,i<=dtarget%5,i++){
+				output=output+one;
+			}
+			return output+exchange(starget);
+		}
+	} 
  }
  private String exchange(int starget,int dtarget,int ttarget){
-	 
+			char one,five,ten;
+	String output = new String();
+	one = 'C';
+	five = 'D';
+    ten = 'M';
+	switch (ttarget){
+	 case 1: output=one;
+	         return output+exchange(dtarget,starget);
+     case 5:output=five;
+	        return output+exchange(dtarget,starget);	 
+	}
+	if (ttarget<5){
+		if(ttarget>3){
+			output = one+five;
+			return output+exchange(dtarget,starget);
+		}else{
+			for (int i = 1,i<=ttarget,i++){
+				output = output + one;
+			}
+			return output+exchange(dtarget,starget);
+		}
+	}else{
+		if (ttarget>8){
+			output = one +ten;
+			return output+exchange(dtarget,starget);
+		}else{
+			output=five;
+			for (int i = 1,i<=ttarget%5,i++){
+				output=output+one;
+			}
+			return output+exchange(dtarget,starget);
+		}
+	}  
  }
  private String checkdigit(Integer target) {
    if (target<10){
