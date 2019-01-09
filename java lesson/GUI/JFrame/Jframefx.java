d6bb97ad077285682807eba6203205b0144b2e37
decemberC <wamghoc71@gmail.com>
2019-01-10 00:13:08 +0800

    GUI:Jframefx is ok now(open two screen,named and unnamed)

--- java lesson/GUI/JFrame/Jframefx.java
+++ java lesson/GUI/JFrame/Jframefx.java
@@ -5,7 +5,13 @@ import javafx.stage.Stage;
 public class Jframefx extends Application{
   @Override
 	public void start(Stage stage){
+		Stage namedstage = new Stage();
 		Scene screen = new Scene(new FlowPane(),640,300);
+		Scene namedscene = new Scene(new FlowPane(),640,300);
+		namedstage.setTitle("Hello world!");
+		namedstage.setScene(namedscene);
+		namedstage.show();
+		namedstage.setFullScreen(false);
 		stage.setScene(screen);
 		stage.show();
 	}
