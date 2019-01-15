fe79c19114d5d77b6ed9a142f273062439ef8ccc
decemberC <wanghoc71@gmail.com>
2019-01-15 11:17:16 +0800

    Update Jbuttonfx.java
    add Jbuttonfx.java(not complete)

--- java lesson/GUI/Jbutton/Jbuttonfx.java
+++ java lesson/GUI/Jbutton/Jbuttonfx.java
@@ -1,3 +1,18 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.acene.control.Button;
+improt javafx.scene.layout.*;
+import java.awt.*;
+
+public class Jbuttonfx extends Application{
+ @Override
+  public void start(Stage primarystage){
+    Dimension screensize = new Dimension();
+    Button bu = new Button();
+    Scene base = new Scene(FlowPane(bu),);
+  }
+  public static void main(String[] args){
+   launch();
+  }
+}
