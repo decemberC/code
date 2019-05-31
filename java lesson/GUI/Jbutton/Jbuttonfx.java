d17c75379c83ee16da3047d515f8237ce82dcb37
decemberC <wamghoc71@gmail.com>
2019-05-31 23:49:05 +0800

    CPP:add a new lab exercise(lab04,try to do the image import and following) java: finish jbuttonfx.java

--- java lesson/GUI/Jbutton/Jbuttonfx.java
+++ java lesson/GUI/Jbutton/Jbuttonfx.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.acene.control.Button;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import java.awt.*;
 
@@ -10,7 +10,9 @@ public class Jbuttonfx extends Application{
   public void start(Stage primarystage){
     Dimension screensize = new Dimension();
     Button bu = new Button();
-    Scene base = new Scene(FlowPane(bu),);
+    Scene base = new Scene(new FlowPane(bu),1440,900);
+	primarystage.setScene(base);
+	primarystage.show();
   }
   public static void main(String[] args){
    launch();
