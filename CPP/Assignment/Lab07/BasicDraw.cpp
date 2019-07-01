3796b2e769c8ab931972e3668c3653a4eab42518
decemberC <wamghoc71@gmail.com>
2019-07-02 02:03:12 +0800

    add more thing of drawing grab

--- CPP/Assignment/Lab07/BasicDraw.cpp
+++ CPP/Assignment/Lab07/BasicDraw.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e944add244cfac49908017bfeee2d48eeed71e0e656000ebe93d1a1d3a4fd564
-size 939
+oid sha256:51e00647d28d4e9fe76032620d06b831c7055964e0f06c37a269f568e7c00dd1
+size 998
